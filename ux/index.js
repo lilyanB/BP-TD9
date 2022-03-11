acaacf1e164553fc9860518542ee205ad865b29f
lilyan <lilyan.bastien@gmail.com>
2022-03-11 14:15:53 +0100

    init2

--- ux/index.js
+++ ux/index.js
@@ -17,30 +17,11 @@ app.set('view engine', 'ejs')
 const postsRoute = require('./routes/posts');
 app.use('/', postsRoute);
 
-/*
-//post
-var path = require('path')
-var bodyParser = require('body-parser');
-app.post('/views/index.ejs', function(req,res){
-    console.log('receiving data ...');
-    console.log('body is ',req.body);
-    res.send(req.body);
-});
-*/
-
-
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-/*
-app.post('/', function(req, res) {
-    console.log('post recu');
-    console.log(req.body.IdTx);
-    res.redirect('posts/' + req.body.IdTx);
-});
-*/
 
 app.use(express.static('public'))
 
